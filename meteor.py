5a8898c18e279c31659c7152c926b887e80999cc
Julian Hitschler <hitschler@cl.uni-heidelberg.de>
2016-09-15 09:21:37 +0200

    Scorer class to avoid overhead

--- meteor.py
+++ meteor.py
@@ -3,40 +3,47 @@
 
 METEOR_PATH="/home/juli/data/programming/meteor-1.5"
 
-import subprocess, shlex
+import subprocess
 from reference import Reference
 
+class MeteorScorer():
+    """
+    Starts a METEOR process and keeps it alive, so that the model can be kept in memeory.
+    """
+    def __init__(self, meteor_language="en", meteor_path="~/meteor"):
+        self.meteor_language = meteor_language
+        self.meteor_path = meteor_path + "/"
+        command = "java -Xmx2G -jar "+self.meteor_path+"meteor-*.jar - - -l "+self.meteor_language+" -stdio"
+        self.meteor_process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        
+    def terminate_process(self):
+        self.meteor_process.terminate()
+        
 class MeteorReference(Reference):
     """
     Python wrapper for the METEOR metric.
     """
 
-    def __init__(self, reference_tokens, meteor_language="en", meteor_path="~/meteor/"):
-        #print "building reference"
+    def __init__(self, reference_tokens, meteor_language="en", meteor_path="~/meteor/", meteor_scorer=None):
         Reference.__init__(self, reference_tokens)
         self.meteor_language = meteor_language
         self.meteor_path = meteor_path + "/"
         self.reference_string = " ".join(reference_tokens)
-        command = "java -Xmx2G -jar "+self.meteor_path+"meteor-*.jar - - -l "+self.meteor_language+" -stdio"
-        #print command
-        #command = shlex.split(command)
-        #print command
-        self.meteor_process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        if meteor_scorer is None:
+            self.meteor_scorer = MeteorScorer(meteor_language=meteor_language, meteor_path=meteor_path)
+        else:
+            self.meteor_scorer = meteor_scorer
+        #command = "java -Xmx2G -jar "+self.meteor_path+"meteor-*.jar - - -l "+self.meteor_language+" -stdio"
+        #self.meteor_process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
     def score(self, hypothesis_tokens):
-        #return 100.0 if self._reference_tokens == hypothesis_tokens else 0.0
-        #print "scoring"
         reference_string = " ".join(hypothesis_tokens)
-        #std_out, std_err = self.meteor_process.communicate(input="SCORE ||| reference 1 words ||| reference n words ||| hypothesis words")
-        self.meteor_process.stdin.write("SCORE ||| "+self.reference_string+" ||| "+reference_string+"\n")
-        std_out = self.meteor_process.stdout.readline()
-        self.meteor_process.stdin.write("EVAL ||| "+std_out)
-        std_out = self.meteor_process.stdout.readline()
+        self.meteor_scorer.meteor_process.stdin.write("SCORE ||| "+self.reference_string+" ||| "+reference_string+"\n")
+        std_out = self.meteor_scorer.meteor_process.stdout.readline()
+        self.meteor_scorer.meteor_process.stdin.write("EVAL ||| "+std_out)
+        std_out = self.meteor_scorer.meteor_process.stdout.readline()
         return float(std_out)
-        #print std_err
-        #self.meteor_process.close()
-        #return 66.0
-
+    
 if __name__ == "__main__":
     ref = MeteorReference(["This", "is", "a", "small", "drink", "."], meteor_path=METEOR_PATH)
     print ref.score(["This", "is", "a", "small", "drink", "."])
