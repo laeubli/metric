d690f4c9994f08b11480cf2feb2a457ed6891d20
Julian Hitschler <hitschler@cl.uni-heidelberg.de>
2016-09-15 18:35:45 +0200

    scorer object for METEOR

--- meteor.py
+++ meteor.py
@@ -9,14 +9,34 @@ from reference import Reference
 class MeteorScorer():
     """
     Starts a METEOR process and keeps it alive, so that the model can be kept in memeory.
+    
+    Arguments are the meteor language abbreviation and the path to the METEOR installation. They need to be specified as follows:"meteor_language=lg,meteor_path=path"
     """
-    def __init__(self, meteor_language="en", meteor_path="~/meteor"):
+    def __init__(self, argument_string):
+        
+        arguments = {}
+        argument_strings = argument_string.split(",")
+        for a in argument_strings:
+            argument, value = a.split("=")
+            argument = argument.strip()
+            value = value.strip()
+            arguments[argument] = value
+        
         self.lock = threading.Lock()
-        self.meteor_language = meteor_language
-        self.meteor_path = meteor_path + "/"
+        self.meteor_language = arguments["meteor_language"]
+        self.meteor_path = arguments["meteor_path"] + "/"
         command = "java -Xmx2G -jar "+self.meteor_path+"meteor-*.jar - - -l "+self.meteor_language+" -stdio"
         self.meteor_process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         
+    def set_reference(self, reference_tokens):
+        self.reference = MeteorReference(reference_tokens, self)
+        
+    def score(self, hypothesis_tokens):
+        return self.reference.score(hypothesis_tokens)
+    
+    def score_matrix(self, hypothesis_matrix):
+        return self.reference.score_matrix(hypothesis_matrix)
+    
     def terminate_process(self):
         self.meteor_process.terminate()
         
@@ -25,17 +45,10 @@ class MeteorReference(Reference):
     Python wrapper for the METEOR metric.
     """
 
-    def __init__(self, reference_tokens, meteor_language="en", meteor_path="~/meteor/", meteor_scorer=None):
+    def __init__(self, reference_tokens, meteor_scorer):
         Reference.__init__(self, reference_tokens)
-        self.meteor_language = meteor_language
-        self.meteor_path = meteor_path + "/"
         self.reference_string = " ".join(reference_tokens)
-        if meteor_scorer is None:
-            self.meteor_scorer = MeteorScorer(meteor_language=meteor_language, meteor_path=meteor_path)
-        else:
-            self.meteor_scorer = meteor_scorer
-        #command = "java -Xmx2G -jar "+self.meteor_path+"meteor-*.jar - - -l "+self.meteor_language+" -stdio"
-        #self.meteor_process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        self.meteor_scorer = meteor_scorer
 
     def score(self, hypothesis_tokens):
         reference_string = " ".join(hypothesis_tokens)
@@ -48,13 +61,14 @@ class MeteorReference(Reference):
         return float(std_out)
     
 if __name__ == "__main__":
-    ref = MeteorReference(["This", "is", "a", "small", "drink", "."], meteor_path=METEOR_PATH)
-    print ref.score(["This", "is", "a", "small", "drink", "."])
-    print ref.score_matrix([
+    scorer = MeteorScorer("meteor_language=en,meteor_path="+METEOR_PATH)
+    scorer.set_reference(["This", "is", "a", "small", "drink", "."])
+    print scorer.score(["This", "is", "a", "small", "drink", "."])
+    print scorer.score_matrix([
         ["This", "is", "a", "big", "drink", "."],
         ["This", "is", "a", "small", "drink", "."]
     ])
-    print ref.score_matrix([
+    print scorer.score_matrix([
         ["This", "is", "a", "big", "drink", "."],
         ["This", "is", "a", "small", "drink", "."]
     ])
